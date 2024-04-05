2b2118e5fa951b67426e93e2d0cefdfc30619d43
cwhaley9 <cwhaley9@unc.edu>
2024-04-04 22:25:30 -0400

    create api get method to retrieve books from genre

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express(); // start server
 
 const port = 3000; // listen on this port
 
-const key = AIzaSyA2-opOfeeNw2MItfgSrAxP9rtmmSbKYWs; // api key for google books
+const key = 'AIzaSyA2-opOfeeNw2MItfgSrAxP9rtmmSbKYWs'; // api key for google books
 
 app.use(express.static('public')); // serve static files
 
@@ -18,6 +18,20 @@ app.listen(port, () => {
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/genre-search', async (req,res) => {
+
+    let genre = 'GENRE PLACEHOLDER';
+    let maxResults = 32;
+
+    // Note: q= filters for word or words in title
+
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&printType=books&maxResults=${maxResults}&key=${key}`);
+
+    let jsonResponse = await response.json(); // await response parsed to json
+
+    res.send(jsonResponse);
 })
 
 
