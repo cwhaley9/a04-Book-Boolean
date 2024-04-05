7a5416914f46f7d5f57512638a6364487d7658ac
cwhaley9 <cwhaley9@unc.edu>
2024-04-04 23:01:47 -0400

    create search button which flips on/off with text input

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,33 @@
+let genreText = document.getElementById('book-genre');
+let searchButtonActive = false;
+
+document.addEventListener('DOMContentLoaded', () => { 
+    onDomLoaded(); 
+});
+
+function onDomLoaded(){
+    genreText.addEventListener('input', () => {
+        alterSearchButton();
+    })
+}
+
+function alterSearchButton(){
+    if(genreText.value.trim() == '' && searchButtonActive){
+        let searchButton = document.getElementById('search-button');
+        searchButton.remove();
+        searchButtonActive = false;
+    }
+    else{
+        if(!searchButtonActive){
+            let searchButton = document.createElement('button');
+            searchButton.setAttribute('id', 'search-button');
+            let genreContainer = document.getElementsByClassName('genre-input-container')[0];
+            genreContainer.appendChild(searchButton);
+            searchButtonActive = true;
+        }
+    }
+}
+
 // def: removes placeholder from input when in focus (when clicked on)
 function removePlaceholder(){
     document.getElementById('book-genre').removeAttribute('placeholder');
@@ -12,4 +42,8 @@ function removePlaceholder(){
 
 function setPlaceholder(placeholder){
     document.getElementById('book-genre').placeholder = placeholder;
+}
+
+function onGenreSearch(genre){
+
 }
