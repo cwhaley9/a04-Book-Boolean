e9192ade2a160c7821624334c59982f28be722f7
cwhaley9 <cwhaley9@unc.edu>
2024-04-05 20:26:49 -0400

    fix swagger api docs

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const path = require('path');
 const app = express(); // start server
 
 const swaggerUI = require('swagger-ui-express');
-const swaggerDoc = require('./swagger.json');
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc)); // setup swagger UI to test APIs
+const swaggerJSDoc = require('swagger-jsdoc');
+
+// Define options for swagger-jsdoc
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+        title: 'Book Boolean API',
+        version: '1.0.0',
+        description: 'Test fetches coming from Google Books APIs',
+        },
+    },
+    apis: ['./index.js'], // Path to the API routes in your project
+};
+
+const specs = swaggerJSDoc(options); // initialize swaggerJSDoc
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs)); // setup swagger UI to test APIs
+
 
 const port = 3000; // listen on this port
 
@@ -18,12 +34,27 @@ app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
 
-//app.use(cors()); // .use(cors()) applies to all methods, no path applies to all paths. ONLY NEED THIS IF USING MULTIPLE PORTS
-
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+/**
+ * @swagger
+ * /genre-search/{genre}:
+ *   get:
+ *     summary: Search for books by genre
+ *     description: Retrieve a list of books based on the specified genre.
+ *     parameters:
+ *       - in: path
+ *         name: genre
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The genre to search for.
+ *     responses:
+ *       '200':
+ *         description: A list of books matching the specified genre.
+ */
 app.get('/genre-search/:genre', async (req,res) => {
 
     let genre = req.params.genre;
